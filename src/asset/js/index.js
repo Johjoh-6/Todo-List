ca941e4a492638f235736e4e6738ae3c3c2f0f82
Johann six <six.johann@gmail.com>
2022-01-04 18:03:04 +0100

    design done, html and css done

--- src/asset/js/index.js
+++ src/asset/js/index.js
@@ -5,12 +5,5 @@ import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 
 const main = document.querySelector('#main');
-function component() {
-    const element = document.createElement('div');
-  element.setAttribute('id', 'try');
-    element.innerHTML = 'Hello webpack';
+
   
-    return element;
-  }
-  
-  main.appendChild(component());
